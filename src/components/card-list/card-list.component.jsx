f7f3220e6a4a77ddce226b535788b657d196874f
Clyde Dean III <144478028+VoidShamans@users.noreply.github.com>
2024-12-13 13:40:56 -0500

    bug fixed
    implicit return in card-list-component needed a paratheses

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,13 +1,13 @@
 import Card from "../card/card.component";
 import "./card-list.styles.css";
 
-const CardList = ({ monsters }) => {
+const CardList = ({ monsters }) => (
   <div className='card-list'>
     {monsters.map((monster) => {
       console.log(monster, "monster");
-      return <Card monster={monster} />;
+      return <Card key={monster.id} monster={monster} />;
     })}
-  </div>;
-};
+  </div>
+);
 
 export default CardList;
